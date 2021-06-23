13ba092efa8871784099d0a5eb2338fd52ff2a0a
LoganTann <logon313@hotmail.fr>
2021-06-23 13:09:12 +0200

    first codei commit

---
+++ framework-4.1.3/app/Views/templates/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="fr"> <?php // TODO: locale ?>
+  <head>
+    <meta charset="utf-8">
+    <title><?= esc($title) ?></title>
+  </head>
+  <body>
+    <h1><?= esc($title) ?></h1>
